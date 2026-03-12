d58433fddd43554bc351cc73dafce3f7620d8465
Abdelrahman Salama <abdoabdoslama509@gmail.com>
2026-03-12 17:10:28 +0200

    fix zones in the AKS

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -25,7 +25,7 @@ resource "azurerm_kubernetes_cluster" "aks-cluster" {
   default_node_pool {
     name       = "defaultpool"
     vm_size    = "Standard_DS2_v2"
-    zones   = [1, 2, 3]
+    # zones   = [1, 2, 3]
     auto_scaling_enabled = true
     max_count            = 3
     min_count            = 1
