384b1f3825c28225fe3e636caef340ca4bd31cc5
abdoslama0079 <abdoabdoslama509@gmail.com>
2026-03-12 14:45:03 +0200

    Update variables.tf

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -1,17 +1,25 @@
 variable "location" {
-
+  type    = string
+  default = "australiaeast"
 }
- variable "resource_group_name" {}
+ variable "resource_group_name" {
+    type        = string
+  description = "resource group name"
+  default = "aksmern"
+ }
 
 variable "service_principal_name" {
-  type = string
+    type = string
+  default = "jenkinsservice"
 }
 
 variable "ssh_public_key" {
   default = "~/.ssh/id_rsa.pub"
 }
 
-variable "client_id" {}
+variable "client_id" {
+  default = "0000"
+}
 variable "client_secret" {
   type = string
   sensitive = true
@@ -19,4 +27,5 @@ variable "client_secret" {
 
 variable "azurerm_container_registry" {
   type = string
+  default = "azrmregistry"
 }
