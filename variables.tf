b815eae43500c11441c442548271302712769654
Abdelrahman Salama <abdoabdoslama509@gmail.com>
2026-03-12 16:57:19 +0200

    remove text

--- variables.tf
+++ variables.tf
@@ -18,12 +18,3 @@ variable "keyvault_name" {
   type = string
   default = "meranapp002833"
 }
-
-<<<<<<< HEAD
-variable "SUB_ID" {
-  type = string
-  default = data.azurerm_subscription.current.id
-}
-=======
-
->>>>>>> 859873cc8f009f2119309a644990d32a06d94201
