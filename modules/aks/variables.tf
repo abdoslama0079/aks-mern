96fa894f58fe217b9d77069a9887e919dbefba09
abdoslama0079 <abdoabdoslama509@gmail.com>
2026-03-12 15:03:27 +0200

    Update variables.tf

--- modules/aks/variables.tf
+++ modules/aks/variables.tf
@@ -1,38 +1,11 @@
-variable "location" {
-  type    = string
-  default = "australiaeast"
-}
+variable "location" {}
 
-variable "resource_group_name" {
-  type        = string
-  description = "The name of the resource group"
-  default     = "aksmern"
-}
+variable "resource_group_name" {} # This MUST exist here
 
-variable "client_id" {
-  type    = string
-  default = "0000"
-}
+variable "client_id" {}
 
-variable "client_secret" {
-  type      = string
-  sensitive = true
-}
+variable "client_secret" {}
 
-variable "azurerm_container_registry" {
-  type        = string
-  description = "The ID of the ACR"
-  default     = "azrmregistry"
-}
+variable "azurerm_container_registry" {}
 
-# This isn't used in your current main.tf because we switched to tls_private_key
-# but keeping it doesn't hurt.
-variable "ssh_public_key" {
-  type    = string
-  default = "~/.ssh/id_rsa.pub"
-}
-
-variable "service_principal_name" {
-  type    = string
-  default = "jenkinsservice"
-}
+variable "service_principal_name" {}
