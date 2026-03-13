9347f914867aa8b15c3046bc9487b22de8b7f8bf
abdoslama0079 <abdoabdoslama509@gmail.com>
2026-03-13 03:01:36 +0200

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -68,9 +68,10 @@ resource "azurerm_kubernetes_cluster" "aks-cluster" {
 
 
 resource "azurerm_role_assignment" "example" {
-  principal_id                     = azurerm_kubernetes_cluster.aks-cluster.identity[0].principal_id
+  principal_id                     = azurerm_kubernetes_cluster.aks-cluster.identity[0].prins
   role_definition_name             = "AcrPull"
   scope                            = var.azurerm_container_registry
   skip_service_principal_aad_check = true
   depends_on = [azurerm_kubernetes_cluster.aks-cluster]
 }
+
